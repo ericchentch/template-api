1cd345fc4faec1cc47eb7fa0f0ce16f03fe92cdb
tungjj <tungplatin@gmail.com>
2022-10-03 13:41:41 +0700

    delete: comment

--- src/main/java/edunhnil/project/forum/api/dao/fileRepository/FileRepositoryImpl.java
+++ src/main/java/edunhnil/project/forum/api/dao/fileRepository/FileRepositoryImpl.java
@@ -16,15 +16,8 @@ public class FileRepositoryImpl extends AbstractMongoRepository implements FileR
         authenticationTemplate.save(file);
     }
 
-    // @Override
-    // public void deleteFile(File file) {
-    //     // TODO Auto-generated method stub
-    //     authenticationTemplate.save(file);
-    // }
-
     @Override
     public Optional<List<File>> getFiles(Map<String, String> allParams, int page, int pageSize) {
-        // TODO Auto-generated method stub
         Query query = generateQueryMongoDB(allParams, File.class, "", "", page, pageSize);
         Optional<List<File>> total = replaceFind(query, File.class);
         return total;
@@ -32,7 +25,6 @@ public class FileRepositoryImpl extends AbstractMongoRepository implements FileR
 
     @Override
     public Optional<File> getFileById(String _id) {
-        // TODO Auto-generated method stub
         File file = authenticationTemplate.findById(_id, File.class);
         return Optional.ofNullable(file);
     }
